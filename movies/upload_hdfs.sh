95f11e3bda19dc66896478648a9f5e1033af3e96
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2021-03-06 04:22:33 +0200

    Modified bash shell scripts

--- movies/upload_hdfs.sh
+++ movies/upload_hdfs.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-hadoop fs -mkdir hdfs://master:9000/movies ||
+hadoop fs -mkdir hdfs://master:9000/movies &&
 
-hadoop fs -put movies.csv hdfs://master:9000/movies/. ||
-hadoop fs -put movie_genres.csv hdfs://master:9000/movies/. ||
-hadoop fs -put ratings.csv hdfs://master:9000/movies/. ||
+hadoop fs -put movies.csv hdfs://master:9000/movies/. &&
+hadoop fs -put movie_genres.csv hdfs://master:9000/movies/. &&
+hadoop fs -put ratings.csv hdfs://master:9000/movies/. &&
 
-spark-submit csv2parquet.py movies ||
-spark-submit csv2parquet.py movie_genres ||
-spark-submit csv2parquet.py ratings ||
+spark-submit csv2parquet.py movies &&
+spark-submit csv2parquet.py movie_genres &&
+spark-submit csv2parquet.py ratings &&
 
 hadoop fs -ls hdfs://master:9000/movies/.
---
+++ queries/run_queries.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+spark-submit rdd/q1_rdd.py &&
+spark-submit rdd/q4_rdd.py
