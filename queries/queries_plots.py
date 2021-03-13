5c5743e42431f1e937ad5f23606ad63a9edb8a76
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2021-03-13 03:01:08 +0200

    Modified plots with pandas

--- queries/queries_plots.py
+++ queries/queries_plots.py
@@ -11,42 +11,62 @@ def autolabel(rects):
         ax.annotate('{0:.2f}'.format(height),
                     xy=(rect.get_x() + rect.get_width() / 2, height),
                     xytext=(0, 5),
-                    fontsize=9,
+                    fontsize=8,
                     textcoords="offset points",
                     ha='center', va='center')
 
 times = defaultdict(list)
 
-f = open("output/optimizer_times.txt", "r")
+f = open("output/rdd/times.txt", "r")
 for line in f.readlines():
     info = line.rstrip().split(":")
-    if info[0] == 'Y':
-        label = "Disabled"
-    else:
-        label = "Enabled"
+    label = info[0]
     time = float(info[1])
     times[label].append(time)
 f.close()
 
+f = open("output/sql/csv/times.txt", "r")
+for line in f.readlines():
+    info = line.rstrip().split(":")
+    label = info[0]
+    time = float(info[1])
+    times[label].append(time)
+f.close()
+
+f = open("output/sql/parquet/times.txt", "r")
+for line in f.readlines():
+    info = line.rstrip().split(":")
+    label = info[0]
+    time = float(info[1])
+    times[label].append(time)
+f.close()
 
 labels = list(times.keys())
-xtimes = [item for sublist in times.values() for item in sublist]
+times_per_query = list(times.values())
+all_times = []
+for i in range(len(times_per_query[0])):
+    res = []
+    for query in times_per_query:
+        res.append(query[i])
+    all_times.append(res)
+    
 
-df = pd.DataFrame({ 'Execution time': xtimes }, index=labels)
+df = pd.DataFrame({ 'RDD': all_times[0],'SQL with CSV': all_times[1], 'SQL with Parquet': all_times[2] }, 
+                    index=labels)
 
 # print(df)
 
-ax = df.plot.bar(rot=0, width=0.5, legend=False)
+ax = df.plot.bar(rot=0, width=0.8)
 
 fig = ax.get_figure()
 
-ax.set_title('Execution times with query optimizer enabled and disabled')
+ax.set_title('Execution times grouped by query')
 ax.set_ylabel('Time (sec)')
 ax.grid(b = True, color ='grey', linestyle ='-.', linewidth = 0.5, alpha = 0.2)
 # for s in ['top', 'bottom', 'left', 'right']:
 for s in ['top', 'right']:
     ax.spines[s].set_visible(False)
-# ax.legend()
+ax.legend()
 
 autolabel(ax.patches)
 
