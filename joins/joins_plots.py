de7636c9969fdde8c5bd3a9200a73ba9b466c0e2
Antonis Papaoikonomou <36318735+antonypap@users.noreply.github.com>
2021-03-21 07:02:50 +0200

    Added plot script for join

---
+++ joins/joins_plots.py
@@ -0,0 +1,53 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import defaultdict
+import pandas as pd
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{0:.2f}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 5),
+                    fontsize=9,
+                    textcoords="offset points",
+                    ha='center', va='center')
+
+times = defaultdict(list)
+
+f = open("output/join_times.txt", "r")
+for line in f.readlines():
+    info = line.rstrip().split(":")
+    label = info[0]
+    time = float(info[1])
+    times[label].append(time)
+f.close()
+
+
+labels = list(times.keys())
+xtimes = [item for sublist in times.values() for item in sublist]
+
+df = pd.DataFrame({ 'Execution time': xtimes }, index=labels)
+
+# print(df)
+
+ax = df.plot.bar(rot=0, width=0.5, legend=False)
+
+fig = ax.get_figure()
+
+ax.set_title('Execution times with different join optimizations')
+ax.set_ylabel('Time (sec)')
+ax.grid(b = True, color ='grey', linestyle ='-.', linewidth = 0.5, alpha = 0.2)
+# for s in ['top', 'bottom', 'left', 'right']:
+for s in ['top', 'right']:
+    ax.spines[s].set_visible(False)
+# ax.legend()
+
+autolabel(ax.patches)
+
+fig.tight_layout()
+plt.show()
+
+# fig.savefig("join_plots.png")
